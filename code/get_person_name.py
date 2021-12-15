817080e55d401762039fc9751bd07aa754270c42
ATP <zhangyx0987@gmail.com>
2021-12-15 23:27:36 +0800

    code for pre-processing

---
+++ code/get_person_name.py
@@ -0,0 +1,59 @@
+import spacy
+import json
+
+def get_person_name(file_path):
+    parser = spacy.load("en_core_web_sm")
+    name_list = []
+    with open(file_path, 'r', encoding='utf-8') as inputfd:
+        text = inputfd.readlines()
+        text_iter = iter(text)
+        total_lines = len(text)
+        i = 0
+        line = next(text_iter, None)
+        while line is not None:
+            if (i % 100 == 0):
+                print(f'parsing {i} / {total_lines} lines...')
+
+            # divide paragraphs by >1 continuous '\n'
+            while line == '\n':
+                line = next(text_iter, None)
+                i = i + 1
+            
+            paragraph = []
+            while line is not None and line != '\n':
+                paragraph.append(line)
+                line = next(text_iter, None)
+                i = i + 1
+            paragraph = ''.join(paragraph)
+            
+            # using SpaCy to get entity tokens
+            token_list = parser(paragraph)
+            name = []
+            for token in token_list:
+                if token.ent_type_ != 'PERSON':
+                    if len(name) != 0:
+                        # delete redudant '\n', ' ' and '\'s'
+                        cur_name = ' '.join(name)
+                        cur_name = cur_name.split()
+                        if cur_name[-1] == '\'s':
+                            cur_name = ' '.join(cur_name[:-1])
+                        else:
+                            cur_name = ' '.join(cur_name)
+                        name_list.append(cur_name)
+                        name = []
+                else:
+                    name.append(token.text)
+
+    # unique
+    name_list = list(set(name_list))
+    name_num = len(name_list)
+    print(f'Totally {name_num} names.')
+    return name_list
+
+
+if __name__ == '__main__':
+    text_path = '../data/S-H.txt'
+    output_path = '../data/person_name.json'
+    name_list = get_person_name(text_path)
+    with open(output_path, 'w', encoding='utf-8') as outputfd:
+        json.dump(name_list, outputfd)
---
+++ code/split_chapter.py
@@ -0,0 +1,60 @@
+import json
+from pathlib import Path
+
+def split_chapter(text_path, output_path):
+    assert isinstance(output_path, Path), 'output_path must be a POSIX Path object.'
+    output_path.mkdir(parents=True, exist_ok=True)
+    with open(text_path, 'r', encoding='utf-8') as inputfd:
+        text = inputfd.readlines()
+        text_iter = iter(text)
+        line = next(text_iter, None)
+
+        # get the table of contents
+        while line is not None:
+            if 'Table of contents' in line:
+                line = next(text_iter, None)
+                break
+            line = next(text_iter, None)
+        table_of_contents = []
+        while line is not None:
+            if 'A STUDY IN SCARLET' in line:
+                break
+            title = line.strip()
+            if len(title) > 0:
+                table_of_contents.append(title.upper())
+            line = next(text_iter, None)
+        print(table_of_contents)
+        # there is an epilogue at the end of the text, beginning with '----------'
+        table_of_contents.append('----------')
+
+        # split chapters
+        title_nums = len(table_of_contents)
+        chapter_cnt = 0
+        for i in range(0, title_nums - 1):
+            cur_title = table_of_contents[i]
+            next_title = table_of_contents[i + 1]
+            assert cur_title in line
+            passage = []
+            line = next(text_iter, None)
+            while line is not None and next_title not in line:
+                if len(line.strip()) > 0:
+                    passage.append(line)
+                line = next(text_iter, None)
+            if len(passage) > 0:
+                if cur_title == 'PREFACE':
+                    cur_title = table_of_contents[i - 1] + ":" + cur_title
+                cur_chapter = {
+                    'title': cur_title,
+                    'text': ''.join(passage)
+                }
+                file_name = f'chapter_{chapter_cnt}.txt'
+                file_path = output_path / file_name
+                with open(file_path, 'w', encoding='utf-8') as outputfd:
+                    json.dump(cur_chapter, outputfd)
+                chapter_cnt += 1
+        print(f'Totally {chapter_cnt} chapters.')
+
+if __name__ == '__main__':
+    text_path = Path('../data/S-H.txt')
+    output_path = Path('../data/chapters')
+    split_chapter(text_path, output_path)
